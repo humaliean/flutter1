19205fdf24d88cb80b0610db30ddf9adc2cfdad6
Harshith Krishna <harshithkrishnaps@gmail.com>
2023-05-16 19:28:36 +0530

    Initial commit

---
+++ bin/flutter_project.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_project/flutter_project.dart' as flutter_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_project.calculate()}!');
+}
---
+++ bin/sample_first_program.dart
@@ -0,0 +1,18 @@
+  class Car {
+  String? model ;
+  String? seats ;
+  String? colour;
+
+
+  }
+
+void main() {
+  Car no1 = Car();
+  print('Model name is ${no1.model ='Swift'}' ) ;
+  print('Number of Seats ${no1.seats ='Four'}') ;
+
+  Car no2 = Car();
+  print('Model name is ${no2.model ='Brezza'}' );
+  print('Number of Seats ${no2.seats ='5'}') ;
+
+}
